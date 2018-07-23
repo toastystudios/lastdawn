dfef33438b1a39ee50465140926b6d51bdf531cb
Rui Almeida <ruialmeida198@gmail.com>
2018-07-23 17:51:44 +0100

    bugfix

--- lastdawn/core/src/toastystudios/lastdawn/view/GameScreen.java
+++ lastdawn/core/src/toastystudios/lastdawn/view/GameScreen.java
@@ -26,6 +26,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void show () {
+
+        Gdx.input.setInputProcessor(null);
+        
         // load the koala frame
         playerTexture = new TextureRegion(new Texture("player.png"));
 
